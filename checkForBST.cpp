22390a49990001aaa01e4ade7e73b9d5115ebc18
Ayush-Dabas <aydbs23@gmail.com>
2022-05-11 22:14:08 +0530

    Create checkForBST.cpp

---
+++ checkForBST.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node{
+    int data;
+    node *left;
+    node *right;
+    node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+bool isBST(node *root, node* MIN = NULL, node* MAX = NULL){
+    if(root == NULL)
+        return true;
+
+    if(MIN != NULL && root->data <= MIN->data)
+        return false;
+    if(MAX != NULL && root->data >= MAX->data)
+        return false;
+
+    bool Left = isBST(root->left, MIN, root);
+    bool Right = isBST(root->right, root, MAX);
+
+    return Left and Right;
+}
+
+int main(){
+    cout << "Family guy\n";
+    // node *root = new node(1);
+    // root->left = new node(2);
+    // root->right = new node(3);
+
+    node *root = new node(5);
+    root->left = new node(2);
+    root->right = new node(8);
+
+    if(isBST(root, NULL, NULL))
+        cout << "YO\n";
+    else
+        cout << "NO\n";
+    return 0;
+}
